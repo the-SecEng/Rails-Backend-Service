1fd971146868cf8c8b4724bc370e7e02e4851544
Goktug <ge_dev@yahoo.com>
2023-05-04 18:37:56 -0500

    Cleaned the code
    Cleaned the code

--- app/controllers/procesos_controller.rb
+++ app/controllers/procesos_controller.rb
@@ -1,23 +1,3 @@
-# class ProcesosController < ApplicationController
-#     def show
-#       id = params[:id]
-#       file_path = Rails.root.join('app', 'data', ' ppp_procesos.json')
-#       file_contents = File.read(file_path)
-#       data = JSON.parse(file_contents)
-  
-#       proceso = data.find { |p| p['id'] == id }
-#       if proceso.nil?
-#         render json: { error: 'Proceso not found' }, status: :not_found
-#       else
-#         render json: proceso['proceso']
-#       end
-#     end
-#   end
-  
-
-
-# Test
-
 class ProcesosController < ApplicationController
   def index
     procesos = PppProcesosTest.all
@@ -34,18 +14,19 @@ class ProcesosController < ApplicationController
   end
 end
 
-
-
-
-
 # class ProcesosController < ApplicationController
-#   def show
-#     @proceso = PppProcesosTest.all_columns(id: params[:id])
-#     if @proceso.nil?
-#       render json: { error: 'Proceso not found' }, status: :not_found
-#     else
-#       render json: @proceso
+#     def show
+#       id = params[:id]
+#       file_path = Rails.root.join('app', 'data', ' ppp_procesos.json')
+#       file_contents = File.read(file_path)
+#       data = JSON.parse(file_contents)
+  
+#       proceso = data.find { |p| p['id'] == id }
+#       if proceso.nil?
+#         render json: { error: 'Proceso not found' }, status: :not_found
+#       else
+#         render json: proceso['proceso']
+#       end
 #     end
 #   end
-# end
-
+  
